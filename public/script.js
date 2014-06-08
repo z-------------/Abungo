65dd3f694ce9e8cde91bfcd6beeb044cf61c0d45
Zachary Guard <zacharyguard@gmail.com>
2014-06-08 09:43:05 +0800

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,217 @@
+var socket = io();
+
+var nick;
+
+function encodeHTML(string) {
+    var tempDiv = document.createElement("div");
+    tempDiv.innerText = string;
+    return tempDiv.innerHTML;
+}
+
+(function chooseNick(){
+    var lastNick = localStorage.lastNick || "";
+    nick = encodeHTML(prompt("Choose a nickname",lastNick));
+    if (nick) {
+        socket.emit("nick chosen",nick);
+        localStorage.lastNick = nick;
+        main();
+    } else {
+        //chooseNick();
+        history.back();
+    }
+})();
+
+function main() {
+    var $ = function(selector){return document.querySelector(selector)};
+    var $$ = function(selector){return document.querySelectorAll(selector)};
+    
+    var onlineUsers = [];
+    var typingList = [];
+    
+    Array.prototype.remove = function(element){return this.splice(this.indexOf(element),1)};
+    
+    HTMLElement.prototype.scrollBottom = function(){
+        return this.scrollHeight - this.scrollTop - this.offsetHeight;
+    }
+    
+    var autoLinkOptions = {
+        callback: function(url) {
+            return "<a href='"+url+"' target='_blank'>"+url+"</a>";
+        }
+    }
+    
+    function writeListItem(html,type,time) {
+        var wasAtBottom = $("#messages").scrollBottom() == 0; // boolean
+        
+        var liNode = document.createElement("li");
+        liNode.innerHTML = "<div class='message'>" + html + "</div>";
+        
+        if (type == "self" || type == "status") {
+            liNode.classList.add(type);
+        } else {
+            liNode.classList.add("normal");
+        }
+        /*if (time) {
+            liNode.setAttribute("title",time);
+        }*/
+        if (type == "console") {
+            liNode.classList.add("console");
+        }
+        if (type == "typing") {
+            liNode.classList.add("typing");
+        }
+        
+        $("#messages").appendChild(liNode);
+        
+        if (wasAtBottom || type == "self") {
+            $("#messages").scrollTop = $("#messages").scrollHeight;
+        }
+        
+        if ((!document.hasFocus() || !wasAtBottom) && type != "status" && type != "typing" && type != "self") {
+            playSound("sound/message.ogg");
+        }
+    }
+    
+    function playSound(url) {
+        var audio = document.createElement("audio");
+        audio.src = url;
+        audio.play();
+    }
+    
+    function updateOnlineUsers() {
+        onlineUsers = onlineUsers.sort();
+        $("#users").innerHTML = "<strong>Online users</strong>: " + onlineUsers.join(", ");
+    }
+    
+    function updateTypingList() {
+        var tListKeys = Object.keys(typingList).sort();
+        var typings = $$("li.typing");
+        for (i=0; i<typings.length;i++) {
+            typings[i].parentElement.removeChild(typings[i]);
+        }
+        for (i=0; i<tListKeys.length; i++) {
+            if (typingList[tListKeys[i]] == true) {
+                writeListItem("<strong>" + tListKeys[i] + "</strong> is typing","typing", new Date().toString());
+            }
+        }
+    }
+    
+    // actual script
+    
+    $("#m").focus();
+    
+    onlineUsers.push(nick);
+    updateOnlineUsers();
+    
+    $("form").onsubmit = function(e){
+        e.preventDefault();
+        var text = $("#m").value;
+        if ($("#m").value.split(" ").join("").length != 0) {
+            socket.emit("chat message",{
+                text: text,
+                nick: nick
+            });
+            socket.emit("stopped typing");
+            writeListItem("<strong>" + nick + "</strong>" + encodeHTML(text).autoLink(autoLinkOptions), "self", new Date().toString());
+        }
+        this.reset();
+    }
+    
+    $("#m").onkeyup = function(){
+        if ($("#m").value.length > 0) {
+            socket.emit("typing");
+        } else {
+            socket.emit("stopped typing");
+        }
+    };
+    
+    $("#fileshare input[type=file]").onchange = function(){
+        var file = this.files[0];
+        if (file.size < 2233076) { // ~2mb
+            var reader = new FileReader();
+            reader.onload = function() {
+                if (file.type.indexOf("image/") != -1) { // file is an image
+                    var dataURI = this.result;
+                    socket.emit("image share",{
+                        file: dataURI,
+                        nick: nick,
+                        fileName: file.name
+                    });
+                    writeListItem("<strong>"+nick+"</strong><a href='"+dataURI+"' target='_blank'><img src='"+dataURI+"'/></a>","self",new Date().toString());
+                } else {
+                    var dataURI = this.result;
+                    socket.emit("file share",{
+                        file: dataURI,
+                        nick: nick,
+                        fileName: file.name
+                    });
+                    writeListItem("<strong>"+nick+"</strong><a href='"+dataURI+"' target='_blank'>"+file.name+"</a>","self",new Date().toString());
+                }
+            }
+            reader.readAsDataURL(file);
+        } else {
+            alert("The file you chose is too big. Choose a file less than 100mb in size.");
+        }
+    }
+    
+    socket.on("chat message", function(msg){
+        var text = msg.text;
+        var sender = msg.nick;
+        var time = msg.time;
+        var type;
+        
+        msg.fromConsole == true ? type = "console" : type = null;
+        
+        writeListItem("<strong>"+sender+"</strong>" + encodeHTML(text).autoLink(autoLinkOptions), type, time);
+    });
+    
+    socket.on("user joined", function(data){
+        var nick = data.nick;                    
+        writeListItem("<strong>"+nick+"</strong> joined", "status", data.time);
+        onlineUsers = data.users;
+        updateOnlineUsers();
+    });
+    
+    socket.on("user left", function(data){
+        var nick = data.nick;
+        writeListItem("<strong>"+nick+"</strong> left", "status", data.time);
+        onlineUsers = data.users;
+        updateOnlineUsers();
+    });
+    
+    socket.on("kick", function(reason){
+        alert("You have been kicked: " + reason);
+        history.back();
+    });
+    
+    socket.on("still kicked",function(){
+        alert("Please wait 5 minutes after getting kicked");
+        history.back();
+    });
+    
+    socket.on("typing", function(nick){
+        typingList[nick] = true;
+        updateTypingList();
+    });
+    
+    socket.on("stopped typing",function(nick){
+        typingList[nick] = false;
+        updateTypingList();
+    });
+    
+    socket.on("server stopping",function(){
+        alert("Server is stopping. Your messages will not be sent until the server is back online and you refresh the page.");
+    });
+    
+    socket.on("image share", function(data){
+        writeListItem("<strong>"+data.nick+"</strong><a href='"+data.file+"' target='_blank'><img src='"+data.file+"'/></a>","normal",data.time);
+    });
+    
+    socket.on("file share", function(data){
+        writeListItem("<strong>"+data.nick+"</strong><a href='"+data.file+"' target='_blank'>"+data.fileName+"</a>","normal",data.time);
+    });
+    
+    socket.on("brainwash", function(){
+        $("#messages").innerHTML = "";
+    });
+}
---
+++ server.js
@@ -0,0 +1,124 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+    res.sendfile(__dirname + "/public/index.html");
+});
+
+app.get(/^(.+)$/, function(req, res) {
+    res.sendfile(__dirname + "/public/" + req.params[0]);
+});
+
+Array.prototype.remove = function(index){return this.splice(index,1)};
+
+var connectedUsers = [];
+var nickIdMap = {};
+var clients = {};
+var kickList = []; // a list of kicked ips
+
+io.on("connection", function(socket){
+    var ip = socket.request.connection.remoteAddress;
+    var userNick;
+    var clientId = socket.id;
+    
+    console.log("a user connected from " + ip);
+    
+    socket.on("nick chosen", function(nick){
+        console.log("user at " + ip + " chose nick '" + nick + "'");
+        if (kickList.indexOf(ip) != -1) {
+            socket.emit("still kicked");
+        } else if (connectedUsers.indexOf(nick) != -1) {
+            socket.emit("kick","nick already in use");
+            console.log(nick + " (" + ip + ") was kicked: nick already in use");
+        } else { // nick is not taken and ip isnt on kicklist
+            clients[clientId] = {
+                socket: socket,
+                ip: ip
+            }
+            
+            userNick = nick;
+            connectedUsers.push(nick);
+            nickIdMap[nick] = clientId;
+            
+            io.emit("user joined", {
+                nick: nick,
+                users: connectedUsers,
+                time: new Date().toString()
+            });
+            
+            socket.on("disconnect", function(){
+                clients[clientId] = undefined;
+                connectedUsers.remove(connectedUsers.indexOf(userNick));
+                io.emit("user left", {
+                    nick: userNick,
+                    users: connectedUsers,
+                    time: new Date().toString()
+                });
+                console.log(userNick + " (" + ip + ") " + " disconnected");
+            });
+            
+            socket.on("chat message", function(msg){
+                msg.time = new Date().toString();
+                socket.broadcast.emit("chat message", msg);
+                console.log(msg.nick + " (" + ip + "): " + msg.text);
+            });
+            
+            socket.on("typing",function(){
+                socket.broadcast.emit("typing", nick);
+            });
+            
+            socket.on("stopped typing",function(){
+                socket.broadcast.emit("stopped typing",nick);
+            });
+            
+            socket.on("image share",function(data){
+                data.time = new Date().toString();
+                socket.broadcast.emit("image share",data);
+                console.log(data.nick + " shared an image: " + data.fileName);
+            });
+            
+            socket.on("file share",function(data){
+                data.time = new Date().toString();
+                socket.broadcast.emit("file share",data);
+                console.log(data.nick + " shared a file: " + data.fileName);
+            });
+        }
+    });
+});
+
+http.listen(3000, function(){
+    console.log("listening on *:3000");
+});
+
+
+// listen for admin commands
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (text) {
+    if (text.indexOf("kick:") == 0) {
+        var userNick = text.substring("kick: ".length).replace("\n","");
+        var userId = nickIdMap[userNick];
+        var userIp = clients[userId].ip;
+        clients[userId].socket.emit("kick","King Jimmy has spoken");
+        
+        kickList.push(userIp);
+        setTimeout(function(){
+            kickList.remove(this.indexOf(userIp));
+        },300000);
+        
+        console.log(userNick + " was kicked: King Jimmy has spoken");
+    } else if (text.indexOf("stop") == 0) {
+        io.emit("server stopping");
+        process.exit();
+    } else if (text.indexOf("brainwash") == 0) {
+        io.emit("brainwash");
+    } else {
+        io.emit("chat message", {
+            text: text,
+            nick: "<pre>[console]</pre>",
+            time: new Date().toString(),
+            fromConsole: true
+        })
+    }
+});
